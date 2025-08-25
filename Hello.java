d9b7068f691919f4c96815cc437e12edf8e8f485
ENZZO FERNANDES DAS CHAGAS <25203668@CFP127ALUNO.LOCAL>
2025-08-25 16:25:40 -0300

    Finalizado a aula de GIT

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello{
 	public static void main{
 		System.out.println("Olá, galera!!");
 		System.out.println("Estamos no SENAI!!");
+		System.out.println("O GIT esta funcionando");
 	}
 	
 	
